d3443dc155016d5a23251e9bb5d8b237682471ba
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-02-07 23:26:39 -0400

    add data dynamically

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,4 +1,5 @@
 import '../styles/style.css';
+import './modules/popup.js';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import '@fortawesome/fontawesome-free/css/all.css';
 import './get-shows-api.js';
---
+++ src/assets/scripts/modules/popup.js
@@ -0,0 +1,102 @@
+import axios from 'axios';
+
+const showsList = document.querySelector('.shows-list');
+const baseURL = 'https://api.tvmaze.com/shows/';
+const modal = document.querySelector('.modal');
+
+const popupHtml = (language, name, rating, genres, image, premiered) => {
+  modal.innerHTML = '';
+
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+  modalContent.innerHTML = `
+        <button id="close" class="close-icon-container">
+          <span class="material-symbols-outlined">
+            close
+          </span>
+        </button>
+        <div class="modal-head-container">
+          <div class="movie-img-infos">
+            <img class="movie-img" src="${image.original}"
+              alt="movie-poster" srcset="">
+            <div class="movie-infos-container">
+              <a href="#" class="movie-name">
+                ${name}
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="movie-infos">
+          <div class="language-rating">
+            <p>Language: <span>${language}</span></p>
+            <p>Rating: <span>${rating.average}</span></p>
+          </div>
+          <div class="language-rating">
+            <p>Release Date: ${premiered}</p>
+            <p>Genres: ${genres[0]}</p>
+          </div>
+        </div>
+        <div class="comments-list-add-comment">
+          <div class="comment-list">
+            <h4>Comment ()</h4>
+            <ul class="list">
+              <li>
+                <span class="big">1</span>
+                <div class="img-name">
+                  <span>Username</span>
+                </div>
+                <span class="">This is the comment i add for testing purpose</span>
+              </li>
+            </ul>
+          </div>
+          <div class="add-comment">
+            <h4>Add your comment</h4>
+            <form id="form">
+              <div>
+                <input required type="text" name="username" id="username" placeholder="Your name">
+              </div>
+              <div>
+                <textarea required name="comment" id="comment" placeholder="Add your comment" rows="6"></textarea>
+              </div>
+              <div class="buttonContainer">
+                <button>Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+  `;
+
+  modal.appendChild(modalContent);
+  modal.classList.toggle('hide');
+};
+
+const getPopupInfos = async (id) => {
+  const url = baseURL + id;
+  const { data } = await axios.get(url);
+  const {
+    language, rating, name, genres, image, premiered,
+  } = data;
+  popupHtml(language, name, rating, genres, image, premiered);
+};
+
+const showPopup = async (id) => {
+  getPopupInfos(id);
+};
+
+const showPopupHandler = (target) => {
+  const { id } = target.parentElement?.parentElement;
+  showPopup(id);
+};
+
+showsList.addEventListener('click', (e) => {
+  const { target } = e;
+  if (target.innerText === 'Comments') return showPopupHandler(target);
+});
+
+const hidePopup = () => modal.classList.toggle('hide');
+
+modal.addEventListener('click', (e) => {
+  const { target } = e;
+  if(target?.innerText === 'close') hidePopup();
+});
