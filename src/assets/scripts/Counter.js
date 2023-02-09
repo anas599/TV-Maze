e87fad6c6bfd2390b7b55ec0bb780d0ffd88ccb9
anas599 <anas599@yahoo.com>
2023-02-09 17:45:13 +0300

    Add counter function

--- src/assets/scripts/Counter.js
+++ src/assets/scripts/Counter.js
@@ -1,6 +1,5 @@
-const listCount = document.querySelectorAll('.shows-list li').length;
-function counter() {
+export default function counter() {
+  const listCount = document.querySelectorAll('.shows-list li').length;
   const list = document.getElementById('TV-shows');
   list.innerHTML = `TV Shows(${listCount})`;
 }
-counter();
