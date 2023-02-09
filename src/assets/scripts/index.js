ec160613a16c8d9d142c198f9d1f090ba43494f6
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-02-09 15:46:06 -0400

    add logo

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -6,6 +6,9 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './get-shows-api.js';
 import './fetchLikes-API.js';
 import './likePlus-API.js';
+import logo from '../images/logo.png';
 import counter from './Counter.js';
 
+const imgLogo = document.querySelector('.logo')
+imgLogo.src = logo;
 counter();
